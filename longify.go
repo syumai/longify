192931f942d4cc6c112fb123dca9c499accabe07
syumai <syumai@gmail.com>
2022-05-06 11:26:43 +0900

    create tmpfile only for determine center pos

--- longify.go
+++ longify.go
@@ -9,32 +9,21 @@ import (
 )
 
 func Longify(r io.Reader, w io.Writer, repeatLen, longifiedPos int) error {
-	tmpfile, err := ioutil.TempFile("", "longify")
-	if err != nil {
-		return fmt.Errorf("error creating tmpfile: %w", err)
-	}
-
-	defer os.Remove(tmpfile.Name())
-	defer tmpfile.Close()
-
-	if _, err := io.Copy(tmpfile, r); err != nil {
-		return fmt.Errorf("error copying input to tmpfile: %w", err)
-	}
-
-	if _, err := tmpfile.Seek(0, 0); err != nil {
-		return fmt.Errorf("error seeking tmpfile: %w", err)
-	}
-
 	if longifiedPos == 0 {
-		longifiedPos = getCenterPos(tmpfile)
-	}
-
-	if _, err := tmpfile.Seek(0, 0); err != nil {
-		return fmt.Errorf("error seeking tmpfile: %w", err)
+		centerPos, tmpfile, err := getCenterPos(r)
+		if tmpfile != nil {
+			defer os.Remove(tmpfile.Name())
+			defer tmpfile.Close()
+		}
+		if err != nil {
+			return err
+		}
+		r = tmpfile
+		longifiedPos = centerPos
 	}
 
 	var pos int
-	scanner := bufio.NewScanner(tmpfile)
+	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
 		pos++
 		i := 1
@@ -53,15 +42,29 @@ func Longify(r io.Reader, w io.Writer, repeatLen, longifiedPos int) error {
 	return nil
 }
 
-func getCenterPos(r io.Reader) int {
-	var (
-		lines int
-		err   error
-	)
-	for buf := make([]byte, 1); err != io.EOF; _, err = r.Read(buf) {
+func getCenterPos(r io.Reader) (int, *os.File, error) {
+	tmpfile, err := ioutil.TempFile("", "longify")
+	if err != nil {
+		return 0, nil, fmt.Errorf("error creating tmpfile: %w", err)
+	}
+
+	if _, err := io.Copy(tmpfile, r); err != nil {
+		return 0, tmpfile, fmt.Errorf("error copying input to tmpfile: %w", err)
+	}
+
+	if _, err := tmpfile.Seek(0, 0); err != nil {
+		return 0, tmpfile, fmt.Errorf("error seeking tmpfile: %w", err)
+	}
+
+	var lines int
+	for buf := make([]byte, 1); err != io.EOF; _, err = tmpfile.Read(buf) {
 		if buf[0] == '\n' {
 			lines++
 		}
 	}
-	return lines / 2
+
+	if _, err := tmpfile.Seek(0, 0); err != nil {
+		return 0, tmpfile, fmt.Errorf("error seeking tmpfile: %w", err)
+	}
+	return lines / 2, tmpfile, nil
 }
